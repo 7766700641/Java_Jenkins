2d1331422f1bf86752ec866230c17f73d02efa52
Nikita_Suryavanshi <nikita.a.suryavanshii@gmail.com>
2025-09-04 23:32:01 +0530

    initial commit

--- HelloWjavaorld.java
+++ HelloWjavaorld.java
@@ -1,9 +1,10 @@
 import java.util.Date;
 
-public class HelloWorld {
+public class HelloWjavaorld {
     public static void main(String[] args) {
         System.out.println("This is Java Program");
         System.out.println("output from Java Program");
         System.out.println(new Date());
+        System.out.println("hello");
     }
 }
